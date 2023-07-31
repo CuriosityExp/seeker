42041a4bc1faa25269c18cd03cbc8cc8bc157e0c
CuriosityExp <dsianturi57@gmail.com>
2023-07-31 12:25:16 +0700

    refactor: change port app.js

--- server/apps/app.js
+++ server/apps/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 const cors = require('cors')
 const router = require("./routes")
 
