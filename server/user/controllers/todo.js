adee8ad8e0f9eda20ad4e0433bce9f5b1e15cf4d
MegaBacin <nunubacin20@gmail.com>
2023-08-01 09:13:55 +0700

    feat:added GET

--- server/user/controllers/todo.js
+++ server/user/controllers/todo.js
@@ -1,13 +1,15 @@
 const Todo = require("../mongo-models/todo");
 
 class TodoController {
-    static async createTodo(req, res, next) {
-        try {
-
-        } catch (error) {
-          next(error);
-        }
+    static async getTodo(req, res, next) {
+      try {
+        const { UserId } = req.user;
+        const todos = await Todo.findAll(UserId);
+        res.status(200).json(todos);
+      } catch (error) {
+        next(error);
       }
+    }
 }
 
 module.exports = TodoController;
