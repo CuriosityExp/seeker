f7c00003f0021b1de374044078974d5d9edf91ca
CuriosityExp <dsianturi57@gmail.com>
2023-08-03 15:26:37 +0700

    chore: remove console.log

--- server/user/mongo-models/todo.js
+++ server/user/mongo-models/todo.js
@@ -16,9 +16,7 @@ class TodoList {
 
   static async bulkInsert(todos) {
       const todoCollection = this.todoCollection();
-      console.log(todos);
       const list = await todoCollection.insertMany(todos);
-      console.log(list)
       return list;
   }
 
